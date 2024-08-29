b84467e2f137b59137a70661a75c14a8e68fa259
CHCHALLE <Chrischalle@yahoo.com>
2024-08-29 00:40:06 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -7,26 +7,55 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__="User"
+    id=Column(Integer, primary_key=True)
+    userName=Column(String(255),nullable=False)
+    firstName=Column(String(255),nullable=False)
+    lastName=Column(String(255),nullable=False)
+    email=Column(String(255),nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class CharacterFavorites(Base):
+    __tablename__='CharacterFavorites'
+    id=Column(Integer, primary_key=True)
+    user_id=Column(Integer, ForeignKey('User.id'))
+    user=relationship(User)
+    
+    Characters_id=Column(Integer, ForeignKey('Characters.id'), nullable=False)
 
-    def to_dict(self):
-        return {}
+class PlanetFavorites(Base):
+    __tablename__='PlanetFavorites'
+    id=Column(Integer, primary_key=True)
+    user_id=Column(Integer, ForeignKey('User.id'))
+    user=relationship(User)
+    Planets_id=Column(Integer, ForeignKey('Planets.id'), nullable=False)
+    
 
+
+class Planets(Base):
+    __tablename__='Planets'
+    id=Column(Integer, primary_key=True)
+   
+    name=Column(String(255),nullable=False )
+    size=Column(Integer,nullable=False )
+    population=Column(Integer, nullable=False)
+    
+
+class Characters(Base):
+    __tablename__='Characters'
+
+    id=Column(Integer, primary_key=True)
+    name=Column(String(255),nullable=False )
+    movies=Column(String(255), nullable=False)
+    
+   
+   
+
+    
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem genering the diagram")
+    raise e
